198125be73232cc166b45748d66ca7ceff594f7c
Rory <rory.magee2@gmail.com>
2020-12-04 20:35:59 +0000

    small optmization

--- day1/main.go
+++ day1/main.go
@@ -31,7 +31,7 @@ func readFile() []int {
 func main() {
     inputVals := readFile()
     for i := 0; i < len(inputVals); i++ {
-        for j := 1; j < len(inputVals); j++ {
+        for j := i + 1; j < len(inputVals); j++ {
             if (inputVals[i] + inputVals[j] == 2020) {
                 fmt.Println(inputVals[i] * inputVals[j])
             }
