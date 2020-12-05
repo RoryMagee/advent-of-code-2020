cb13e9773a8fa5194fb01bc21ad8c1232347032c
Rory <rory.magee2@gmail.com>
2020-12-05 20:28:35 +0000

    added solution for day3 part 1

--- day3/main.go
+++ day3/main.go
@@ -9,9 +9,34 @@ import (
 
 func main() {
     inputVals := readFile("./input")
-    for _, v := range inputVals {
-        fmt.Println(v)
+    startPosition := [2]int{0, 0}
+    // startPosition[0] = vertical
+    // startPosition[1] = horizontal
+    // inputvals[row][column]
+    treeCount := 0
+    for startPosition[0] < len(inputVals) {
+        if inputVals[startPosition[0]][startPosition[1]] == "#" {
+            treeCount++
+        }
+        // Increment positions
+        diff := len(inputVals[1]) - (startPosition[1] + 3)
+        if diff <= 0 {
+            startPosition[1] = abs(diff)
+        } else {
+            startPosition[1] = startPosition[1] + 3
+        }
+        startPosition[0] = startPosition[0] + 1
+        fmt.Println("startPosition[0]", startPosition[0], "startPosition[1]",
+        startPosition[1])
     }
+    fmt.Println(treeCount)
+}
+
+func abs(i int) int {
+    if i < 0 {
+        return -i
+    }
+    return i
 }
 
 func readFile(path string) [][]string {
