a3a642fcdf6e58cdbd71431b433b3e7bbce851e0
Rory <rory.magee2@gmail.com>
2020-12-05 23:12:25 +0000

    added solution for part 1 of day4

--- day4/main.go
+++ day4/main.go
@@ -1,9 +1,84 @@
 package main
 
 import (
+	"bufio"
+	"os"
     "fmt"
+    "strings"
 )
 
 func main() {
+    inputVals := readFile("./input")
+    validPassports := 0
+    for _, p := range inputVals {
+        fields := getPassportFields(p)
+        if checkFields(fields) {
+            validPassports++
+        }
+    }
+    fmt.Println(validPassports)
+}
 
+func checkFields(fields []string) bool {
+    required := [7]string {
+        "byr",
+        "iyr",
+        "eyr",
+        "hgt",
+        "hcl",
+        "ecl",
+        "pid",
+    }
+    retValue := true
+    for _, field := range required {
+        if !isValueInArr(fields, field) {
+            retValue = false
+            break
+        }
+    }
+    return retValue
+}
+
+func isValueInArr(arr []string, key string) bool {
+    for _, val := range arr {
+        if val == key {
+            return true
+        }
+    }
+    return false
+}
+
+func getPassportFields(passportDetails string) []string {
+    parsedDetails := strings.Fields(passportDetails)
+    var fields []string
+    for _, field := range parsedDetails {
+        fields = append(fields, field[0:3])
+    }
+    return fields
+}
+
+func readFile(path string) []string {
+    file, e := os.Open(path)
+    check(e)
+    defer file.Close()
+
+    var passports []string
+    scanner := bufio.NewScanner(file)
+    var line string
+    for scanner.Scan() {
+        newLine := scanner.Text()
+        if newLine == "" {
+            passports = append(passports, line)
+            line = ""
+        } else {
+            line = line + " " + newLine
+        }
+    }
+    return passports
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
 }
