ffa8665c939d2cccb60f9b8afa4aae14646bb93f
Rory <rory.magee2@gmail.com>
2020-12-07 21:43:15 +0000

    added solution for day5 part 2

--- day5/main.go
+++ day5/main.go
@@ -9,15 +9,49 @@ import (
 func main() {
     inputVals := readFile("./input")
     highest := 0
+    var allSeatIds []int
     for _, seat := range inputVals {
-        row := weirdBinarySearchTing(127, seat[:7], "F", "B")
-        col := weirdBinarySearchTing(7, seat[7:], "L", "R")
-        result := (row * 8) + col
-        if result > highest {
-            highest = result
+        id := getSeatId(seat)
+        allSeatIds = append(allSeatIds, id)
+        if id > highest {
+            highest = id
+        }
+    }
+    /* 
+    * At this point we will have all the seat ID's
+    * We will need to sort the array and then iterate through and find the 
+    * missing value
+    */
+    sortedIds := sortArr(allSeatIds)
+    for i := 1; i < len(sortedIds) - 1; i++ {
+        if sortedIds[i - 1] != sortedIds[i] - 1 {
+            fmt.Println("solution 2:", sortedIds[i] - 1)
+            break
+        }
+        if sortedIds[i + 1] != sortedIds[i] + 1 {
+            fmt.Println("solution 2:", sortedIds[i] + 1)
+            break
         }
     }
     fmt.Println(highest)
+}
+
+func sortArr(arr []int) []int {
+    for i := 1; i < len(arr); i++ {
+        curr := arr[i]
+        for j := i - 1; j >= 0 && arr[j] > curr; j-- {
+            arr[j + 1] = arr[j]
+            arr[j] = curr
+        }
+    }
+    return arr
+}
+
+func getSeatId(seat string) int {
+    row := weirdBinarySearchTing(127, seat[:7], "F", "B")
+    col := weirdBinarySearchTing(7, seat[7:], "L", "R")
+    result := (row * 8) + col
+    return result
 }
 
 func weirdBinarySearchTing(max int, seq string, upperChar string, lowerChar string) int {
