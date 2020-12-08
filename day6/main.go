875c96906d964786ff95746ccf1bdef3ca40f597
Rory <rory.magee2@gmail.com>
2020-12-08 00:06:04 +0000

    added solution for day 6 part 1

--- day6/main.go
+++ day6/main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
+    inputVals := readFile("./input")
+    fmt.Println(inputVals)
+    total := 0
+    for _, v := range inputVals {
+        total = total + findUnique(v)
+    }
+    fmt.Println(total)
+}
 
+func findUnique(group string) int {
+    m := make(map[string] bool)
+    for _, g := range group {
+        m[string(g)] = true
+    }
+    return len(m)
+}
+
+func readFile(path string) []string {
+    file, err := os.Open(path)
+    check(err)
+    defer file.Close()
+    var str string
+    var groups []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := scanner.Text()
+        if line == "" {
+            groups = append(groups, str)
+            str = ""
+        } else {
+            str = str + line
+        }
+    }
+    groups = append(groups, str)
+    return groups
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
 }
