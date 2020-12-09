5bb924392d2192518c050916822b48993e3958a2
Rory <rory.magee2@gmail.com>
2020-12-09 20:47:27 +0000

    read input file

--- day7/main.go
+++ day7/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"os"
 )
+
+type node struct {
+
+}
 
 func main() {
     fmt.Println("Day 7")
+    inputVals := readFile("./input")
+    for _, line := range inputVals {
+        fmt.Println(line)
+    }
+}
+
+func readFile(path string) []string {
+    file, err := os.Open(path)
+    check(err)
+    defer file.Close()
+    var returnArr []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := scanner.Text()
+        returnArr = append(returnArr, line)
+    }
+    return returnArr
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
 }
