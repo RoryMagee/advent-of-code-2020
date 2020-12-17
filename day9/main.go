2bf575f9ec0140aa8e57d5c4ee0a55ba1315e092
rory-magee <rory.magee2@gmail.com>
2020-12-17 22:28:28 +0000

    update exampole

--- day9/main.go
+++ day9/main.go
@@ -7,12 +7,10 @@ import (
 func main() {
     fmt.Println("day 9")
     arr := make([]int, 10)
-    arr[0] = 1
-    arr[1] = 2
-    arr[2] = 3
-    arr[3] = 4
-    arr[4] = 5
-    for _, i := range arr {
-        fmt.Println(i)
-    }
+    arr[0] = 0
+    arr[1] = 1
+    arr[2] = 2
+    arr[3] = 3
+    arr[4] = 4
+    fmt.Println(arr[0:1])
 }
