d0824e64011c25414f3e02d85b6d61e68650d0b7
rory-magee <rory.magee2@gmail.com>
2020-12-17 22:26:35 +0000

    arr example

--- day9/main.go
+++ day9/main.go
@@ -6,4 +6,13 @@ import (
 
 func main() {
     fmt.Println("day 9")
+    arr := make([]int, 10)
+    arr[0] = 1
+    arr[1] = 2
+    arr[2] = 3
+    arr[3] = 4
+    arr[4] = 5
+    for _, i := range arr {
+        fmt.Println(i)
+    }
 }
