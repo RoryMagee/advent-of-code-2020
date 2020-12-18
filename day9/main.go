8c6309aa41154f082282558169ff1266afa3f575
rory-magee <rory.magee2@gmail.com>
2020-12-18 15:04:48 +0000

    updated main funtion

--- day9/main.go
+++ day9/main.go
@@ -1,9 +1,55 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
+    PREAMBLE_LEN := 25
+    inputVals := readFile("./input")
+    prevIndex := 0
+    for i := PREAMBLE_LEN + 1; i < len(inputVals); i++ {
+        preamble := inputVals[prevIndex: prevIndex + PREAMBLE_LEN]
+        target := inputVals[i]
+        fmt.Println(preamble, target)
+        res := sumExists(preamble, target)
+        if !res {
+            fmt.Println(target)
+            break
+        }
+        prevIndex++
+    }
+}
 
+func readFile(path string) []int {
+    file, err := os.Open(path)
+    check(err)
+    defer file.Close()
+    var returnArr []int
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        value, _ := strconv.Atoi(scanner.Text())
+        returnArr = append(returnArr, value)
+    }
+    return returnArr
+}
+
+func check(e error) {
+    if (e != nil) {
+        panic(e)
+    }
+}
+
+func sumExists(arr []int, target int) bool {
+    for i := 0; i < len(arr); i++ {
+        for j := i+1; j < len(arr); j++ {
+            if arr[i] + arr[j] == target {
+                return true
+            }
+        }
+    }
+    return false
 }
