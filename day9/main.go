26f07dfcc045f5f308e3206a8e6f202f0ce26e26
rory-magee <rory.magee2@gmail.com>
2020-12-18 12:39:40 +0000

    added testinput file for testing

--- day9/main.go
+++ day9/main.go
@@ -5,12 +5,5 @@ import (
 )
 
 func main() {
-    fmt.Println("day 9")
-    arr := make([]int, 10)
-    arr[0] = 0
-    arr[1] = 1
-    arr[2] = 2
-    arr[3] = 3
-    arr[4] = 4
-    fmt.Println(arr[0:1])
+
 }
