845c514d4c865dcf3bcb8230d7d826323b70399e
Rory <rory.magee2@gmail.com>
2020-12-19 23:50:37 +0000

    added starting functions to read input

--- day10/main.go
+++ day10/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
     fmt.Println("Day 10")
+    inputVals := readFile("./testinput")
+    for _, line := range inputVals {
+        fmt.Println(line)
+    }
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func readFile(path string) []int {
+    file, err := os.Open(path)
+    check(err)
+    defer file.Close()
+    var returnArr []int
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        value, _ := strconv.Atoi(scanner.Text())
+        returnArr = append(returnArr, value)
+    }
+    return returnArr
 }
