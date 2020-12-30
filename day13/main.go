38f01a61bd2963e8e648f5275e6485f15f61a3ca
rory-magee <rory.magee2@gmail.com>
2020-12-30 16:35:02 +0000

    added day13

---
+++ day13/main.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+    "fmt"
+)
+
+func main() {
+    fmt.Print("Day 13\n")
+    solution1()
+}
---
+++ day13/solution1.go
@@ -0,0 +1,9 @@
+package main
+
+import(
+    "fmt"
+)
+
+func solution1() {
+    fmt.Print("Solution1\n")
+}
